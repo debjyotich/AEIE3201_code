384176ddc0404f2cb2e00829d93cddfd5296bd57
debjyoti <debjyoti.chowdhury@heritageit.edu>
2022-04-20 11:05:24 +0530

    final read serial code

--- module2/read_serial_data.py
+++ module2/read_serial_data.py
@@ -1,16 +1,15 @@
 import serial
 import time
 import numpy as np
-# import pandas as pd
+import pandas as pd
 
-read_char_num = 7 # dont modify this
+read_char_num = 7  # don't modify this
 
 # can take user input
-no_of_data_points = 10
-delay_time_sec = 3
+no_of_data_points = 20
+delay_time_sec = 1
 serial_port = "/dev/cu.usbmodem14101"
 serial_baud = 9600
-
 
 sensor_data, data_points = [], []  # define empty lists
 ser = serial.Serial(serial_port, serial_baud)
@@ -26,6 +25,9 @@ print("\n")
 data_avg = round(np.average(sensor_data), 2)
 sensor_data_frame = {"S No.": data_points, "Sensor_data": sensor_data}
 
-print(f"The avg. of sensor data {data_avg}")
-print(sensor_data_frame)
+df_w = pd.DataFrame(sensor_data_frame, columns=["S No.", "Sensor_data"])
 
+df_w.to_csv("results.csv", index=False)
+
+# print(f"The avg. of sensor data {data_avg}")
+# print(sensor_data_frame)
